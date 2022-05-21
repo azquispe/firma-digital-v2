d4c7e284c589822e7db3dab62034d33c7d330ff0
alvaroquispesegales <alvaroquispesegales@gmail.com>
2022-05-21 02:36:08 -0400

    se ha implementado validacion con token

---
+++ src/main/java/com/ganaseguro/firmador/security/JwtConfig.java
@@ -0,0 +1,25 @@
+package com.ganaseguro.firmador.security;
+
+public class JwtConfig {
+
+
+    public static final String RSA_PUBLIC="-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvTi3g7xOXzoq6p5fMbHw\n" +
+            "SLu4ja3qYo9RN/wd6LKcKpnzbq3+WBR20rsq/Y6Bq6wG+4cHtw5AfmsJp6xnWPdb\n" +
+            "72qJXFvaOoQw5z1e2mVDe9eko2QlCF+i/nD1cT4VsiBT7pjvc/bpjYPH26F9Mvsa\n" +
+            "15+i2uJxMksvl4ULm1xS0GpE6bgEpR/7B2VpQqaUgrjfZIBzfGC8l4HFCJvpx9SK\n" +
+            "8IEpN0Wfm5cuwYom68O9bUtZl6u07RDBrJEuJacctFVHI6dV0QRBHReWVJyrOsmj\n" +
+            "W/Y9halE+f1kgzA7ijLusHx8YveF2KH7a3eTquwl5uWGp+FGSoQk8257hdypTiAr\n" +
+            "IQIDAQAB\n" +
+            "-----END PUBLIC KEY-----";
+
+    /*public static final String RSA_PUBLIC="-----BEGIN PUBLIC KEY-----\r\n" +
+            "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvJUM7Bvz5hLsQ2hwrmCp\r\n" +
+            "tqB9CjAwoEN8bywZhCSZtigOU/yi0iE9o3Tnxr6M6WVK8XZcP5HdJq58Gyh327xJ\r\n" +
+            "+1Zc49o3KczH9iSu3gTSTNm0zbaRLrkK7a59TLreyNJhS5kDp744p9y9nnwgxfcX\r\n" +
+            "r5Y75omQsI3Q8D6igsp1p94sMwN5o8lx2SNvdZHd/urBsNPI1GzmlEqtLWMHJ+xI\r\n" +
+            "46Ol6Ri7jrs440tSxZ3KHbhgm9LpY4ncxysU7q2M6PJVFZO8QpmJyQ+XJwgmOPzx\r\n" +
+            "Ql0HbL0wHMDyeChzX5rcAQ8i+PLXgc6FrTB5grSaJyRwZmTPVUpEy+D7sLUbdl/r\r\n" +
+            "0wIDAQAB\r\n" +
+            "-----END PUBLIC KEY-----";*/
+}
---
+++ src/main/java/com/ganaseguro/firmador/security/ResourceServerConfig.java
@@ -0,0 +1,57 @@
+package com.ganaseguro.firmador.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/api/**").authenticated();
+    }
+
+
+
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        //jwtAccessTokenConverter.setSigningKey(JwtConfig.RSA_PRIVATE); // solo se usa oara firmar token
+        jwtAccessTokenConverter.setVerifierKey(JwtConfig.RSA_PUBLIC); // llave publica para Validar firma, esta necesitamos
+        return jwtAccessTokenConverter;
+    }
+
+
+}
